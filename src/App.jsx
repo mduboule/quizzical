893c5ec315d9ad7701ea83c04c61abb2315ca01e
Marius Duboule <dev@mariusduboule.com>
2022-05-19 20:46:22 -0400

    Make API call, display questions and anwers randomly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import yellow from './images/yellow.png'
+import blue from './images/blue.png'
+import Questions from './Questions'
+import { useEffect, useState } from 'react'
+
 function App() {
-  return null
+  const [gameOn, setGameOn] = useState(false)
+  const [questions, setQuestions] = useState([])
+
+  function handleStartGame() {
+    setGameOn(true)
+  }
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const result = await fetch('https://opentdb.com/api.php?amount=5')
+      const data = await result.json()
+      console.log(data.results)
+      setQuestions(data.results)
+    }
+    fetchQuestions()
+  }, [])
+
+  return (
+    <main>
+      <img src={blue} className="blue-shape" />
+      <img src={yellow} className="yellow-shape" />
+      {!gameOn && (
+        <>
+          <h1>Quizzical</h1>
+          <p>You think you know it all…</p>
+          <button className="start-game big" onClick={handleStartGame}>Start Quiz</button>
+        </>
+      )}
+      {gameOn && <>
+        <Questions questions={questions} />
+        <button className='medium check-answers'>Check answers</button>
+      </>
+      }
+    </main>
+  )
 }
 
 export default App

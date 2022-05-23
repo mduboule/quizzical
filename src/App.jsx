a461bb300dac27a2e3f88c0eec8e08f3945a3cc4
Marius Duboule <dev@mariusduboule.com>
2022-05-22 23:51:33 -0400

    Refactor styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,69 @@
 import yellow from './images/yellow.png'
 import blue from './images/blue.png'
 import Questions from './Questions'
+import Landing from './Landing'
 import { useEffect, useState } from 'react'
 
 function App() {
   const [gameOn, setGameOn] = useState(false)
   const [questions, setQuestions] = useState([])
+  const [playerAnswers, setPlayerAnswers] = useState([])
 
-  function handleStartGame() {
+  function startGame() {
+    console.log("setGameOn")
     setGameOn(true)
   }
 
+  function selectAnswer(question, answer) {
+    console.log("setPlayerAnswers")
+    setPlayerAnswers(prevAnswers => {
+      const newAnswers = [...prevAnswers]
+      const questionExists = newAnswers.find(answer => answer.question === question)
+      // first time the question is answered
+      if (!questionExists) {
+        console.log("new answer")
+        newAnswers.push({ question: question, answer: answer })
+      }
+      // user clicked on the same answer a second time
+      else if (newAnswers[newAnswers.indexOf(questionExists)].answer === answer) {
+        console.log("remove answer")
+        newAnswers.splice(newAnswers.indexOf(questionExists), 1)
+      }
+      // user changed their answer
+      else {
+        console.log("changed their answer")
+        newAnswers[newAnswers.indexOf(questionExists)].answer = answer
+      }
+      return newAnswers
+    })
+  }
+
   useEffect(() => {
+    console.log("fetching questions")
     const fetchQuestions = async () => {
       const result = await fetch('https://opentdb.com/api.php?amount=5')
       const data = await result.json()
-      console.log(data.results)
       setQuestions(data.results)
     }
     fetchQuestions()
   }, [])
 
   return (
-    <main>
-      <img src={blue} className="blue-shape" />
-      <img src={yellow} className="yellow-shape" />
-      {!gameOn && (
-        <>
-          <h1>Quizzical</h1>
-          <p>You think you know it all…</p>
-          <button className="start-game big" onClick={handleStartGame}>Start Quiz</button>
-        </>
-      )}
-      {gameOn && <>
-        <Questions questions={questions} />
-        <button className='medium check-answers'>Check answers</button>
-      </>
-      }
-    </main>
+    <>
+      <main>
+        {!gameOn && <Landing handleClick={() => startGame()} />}
+        {gameOn &&
+          <>
+            <Questions
+              questions={questions}
+              playerAnswers={playerAnswers}
+              selectAnswer={selectAnswer}
+            />
+            <button className='medium check-answers'>Check answers</button>
+          </>
+        }
+      </main>
+    </>
   )
 }
 

ecd3e9318853af3f8017ca96a5681b5d39c177ae
Marius Duboule <dev@mariusduboule.com>
2022-07-04 21:48:08 +0200

    chore(images): move image folder + cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import yellow from './images/yellow.png'
-import blue from './images/blue.png'
 import Questions from './Questions'
 import Landing from './Landing'
 import { useEffect, useState } from 'react'
@@ -14,24 +12,24 @@ function App() {
     setGameOn(true)
   }
 
+  useEffect(() => {
+    console.log(playerAnswers)
+  }, [playerAnswers])
+
   function selectAnswer(question, answer) {
-    console.log("setPlayerAnswers")
     setPlayerAnswers(prevAnswers => {
       const newAnswers = [...prevAnswers]
       const questionExists = newAnswers.find(answer => answer.question === question)
       // first time the question is answered
       if (!questionExists) {
-        console.log("new answer")
         newAnswers.push({ question: question, answer: answer })
       }
       // user clicked on the same answer a second time
       else if (newAnswers[newAnswers.indexOf(questionExists)].answer === answer) {
-        console.log("remove answer")
         newAnswers.splice(newAnswers.indexOf(questionExists), 1)
       }
       // user changed their answer
       else {
-        console.log("changed their answer")
         newAnswers[newAnswers.indexOf(questionExists)].answer = answer
       }
       return newAnswers
